f6eaf92d780428194f0100e35008ecd0d18e4814
SamiErrighi <sami.errighi@gmail.com>
2014-12-17 16:06:52 +0100

    add send emailing

--- src/User/Bundle/Controller/DefaultController.php
+++ src/User/Bundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace User\Bundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -15,5 +16,34 @@ class DefaultController extends Controller
     public function indexAction($name)
     {
         return array('name' => $name);
+    }
+
+    /**
+     * @Route("/invite/friends")
+     * @Template()
+     */
+    public function inviteAction(Request $request)
+    {
+        $form = $this->createFormBuilder(null)
+            ->add('email', 'text')
+            ->getForm();
+
+        if($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            $email = $form->getData()["email"];
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Hello Email')
+                ->setFrom('carotte@lp-dw.com')
+                ->setTo($email)
+                ->setBody($this->renderView('@User/Email/email.txt.twig', array('email' => $email)))
+            ;
+            $this->get('mailer')->send($message);
+
+            return $this->redirect($this->generateUrl('gladiator__gladiator_index'));
+        }
+
+        return [
+            "form" => $form->createView()
+        ];
     }
 }
